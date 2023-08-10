5b6a94d0324ad056f82e003502d58e17131abba2
frhl <frederik.heymann@gmail.com>
2023-08-10 11:32:47 +0100

    added genebass vcf

--- scripts/00_filter_genebass.sh
+++ scripts/00_filter_genebass.sh
@@ -9,8 +9,7 @@
 #SBATCH --cpus-per-task 1
 #SBATCH --array=21
 
-set -o errexit
-set -o nounset
+set -eu
 
 source utils/qsub_utils.sh
 
@@ -20,10 +19,12 @@ readonly array_idx=$( get_array_task_id )
 readonly chr=$( get_chr ${array_idx} )
 
 readonly in_dir="data-ukb"
-readonly in="${in_dir}/ukb_wes_450k.qced.chr${chr}.vcf"
+readonly in="${in_dir}/ukb_wes_450k.qced.chr${chr}.vcf.gz"
 
-readonly out_dir="data/vep/vep-in"
+readonly out_dir="data/vep-in"
 readonly out_prefix="${out_dir}/genebase.chr${chr}"
+
+echo "ok"
 
 mkdir -p ${out_dir}
 
@@ -34,7 +35,7 @@ zcat ${genebass_variants} | awk ' {print $1":"$2":"$3":"$4}' > "${variants}"
 
 module load BCFtools/1.17-GCC-12.2.0
 bcftools view -i "ID=@${variants}" ${in} -Oz -o ${out_prefix}.vcf.gz
-
+bcftools index ${out_prefix}.vcf.gz
 
 
 
