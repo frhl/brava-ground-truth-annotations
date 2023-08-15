096e00fb144eefdf33009363e31ad76fa1ed4bb2
Barney Hill <barneyhill1@gmail.com>
2023-08-15 14:25:16 +0100

    gnomad maf broken - added in merging from dbnsfp and filtering

--- scripts/02_process_csq.py
+++ scripts/02_process_csq.py
@@ -44,6 +44,29 @@ def main(args):
     ht = hl.read_table(input_path)
     ht = convert_revel_scores_to_unique_float(ht)
     ht = process_consequences(ht)
+
+    dbNSFP = hl.import_table(f'/well/lindgren/barney/brava_annotation/misc/bNSFP4.3a_chr21.bgz', no_header=True)
+    dbNSFP = dbNSFP.rename({"f0":"#chr","f1":"pos(1-based)","f2":"ref","f3":"alt", "f4":"gnomAD_exomes_POPMAX_AF"})
+    dbNSFP = dbNSFP.annotate(pos_int = hl.int(dbNSFP['pos(1-based)']))
+
+    # Add 'chr' prefix to the chromosome field
+    dbNSFP = dbNSFP.annotate(chr_prefixed = hl.str('chr') + dbNSFP['#chr'])
+
+    # Now, create a locus<GRCh38> field
+    dbNSFP = dbNSFP.annotate(locus = hl.locus(dbNSFP.chr_prefixed, dbNSFP.pos_int, reference_genome='GRCh38'))
+
+    # Annotate combined_table with dbNSFP info
+    ht = ht.key_by('locus', 'alleles')
+    dbNSFP = dbNSFP.key_by('locus', 'ref', 'alt')
+
+    # Merge the tables based on key
+    ht = ht.annotate(gnomAD_exomes_POPMAX_AF = dbNSFP[ht.locus, ht.alleles[0], ht.alleles[1]].gnomAD_exomes_POPMAX_AF)
+
+    # Filter down the table
+    ht = ht.filter(hl.is_missing(ht.gnomAD_exomes_POPMAX_AF) |
+                                     (ht.gnomAD_exomes_POPMAX_AF == ".") |
+                                     (hl.float(ht.gnomAD_exomes_POPMAX_AF) < 0.01))
+
     ht.write(out_prefix + ".ht", overwrite=True)
 
 
